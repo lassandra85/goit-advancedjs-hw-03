24241140dc54d2b2b27118a2d40e2f2e8f7409a2
lassandra85 <lassandra85@gmail.com>
2025-06-21 20:18:47 +0200

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '...'; //чекаю на відповідь
+const API_KEY = '31963270-0e971a3d68be2b002dee093f0';
 
 export function fetchImages(query) {
   const params = new URLSearchParams({
